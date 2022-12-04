0634d14b3f6d311c131ae4c3ac45c2dd5ed7bd83
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-12-04 22:36:27 +0000

    fix env vars

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,8 @@ import { LocalUserProvider } from './LocalUserProvider';
 import { theme } from './theme';
 
 const supabaseClient = createClient(
-  import.meta.env.SUPABASE_URL ?? 'http://localhost:54321',
-  import.meta.env.SUPABASE_ANON_KEY ??
+  import.meta.env.VITE_SUPABASE_URL ?? 'http://localhost:54321',
+  import.meta.env.VITE_SUPABASE_ANON_KEY ??
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 );
 
