f96b31b11577201e514af146d7a3ec21ee89480e
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-12-08 17:37:07 +0000

    add print job creation

--- netlify/functions/complete-checkout.ts
+++ netlify/functions/complete-checkout.ts
@@ -1,6 +1,7 @@
 /* eslint-disable unicorn/filename-case */
 import { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
 import { createClient } from '@supabase/supabase-js';
+import axios from 'axios';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(
@@ -30,7 +31,39 @@ const handler: Handler = async (event: HandlerEvent, _context: HandlerContext) =
     const session = evt.data.object;
     const orderId = session.client_reference_id;
 
-    await client.from('orders').update({ stripe_payment_intent_id: session.payment_intent }).eq('id', orderId);
+    const { data: order } = await client
+      .from('orders')
+      .update({ stripe_payment_intent_id: session.payment_intent })
+      .eq('id', orderId)
+      .select('*')
+      .single();
+    const { data: asset } = await client.from('assets').select('*').eq('id', order.asset_id).single();
+
+    const coverImageUrl = await client.storage.from('assets').createSignedUrl(asset.storage_key, 60 * 60 * 12);
+
+    const res = await axios.post(
+      'https://api.replicate.com/v1/predictions',
+      {
+        version: '42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b',
+        input: {
+          image: coverImageUrl.data?.signedUrl,
+          scale: 2105 / 768,
+        },
+        webhook_completed: `${process.env.URL}/.netlify/functions/generate-print-job-background?orderId=${orderId}`,
+      },
+      {
+        headers: {
+          Authorization: `Token ${process.env.REPLICATE_API_KEY}`,
+        },
+      }
+    );
+
+    if (res.status > 299) {
+      throw new Error('Failed to create upscale job');
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(`Upscale job created for ${orderId}`);
   }
 
   return {
---
+++ netlify/functions/generate-print-job-background.ts
@@ -0,0 +1,124 @@
+/* eslint-disable unicorn/filename-case */
+import { HandlerEvent, HandlerContext, BackgroundHandler } from '@netlify/functions';
+import { createClient } from '@supabase/supabase-js';
+import axios from 'axios';
+import path from 'node:path';
+import sharp from 'sharp';
+
+const GREETING_CARD_SKU = 'CLASSIC-GRE-FEDR-7X5-BLA';
+const SHIPPING_METHOD = 'Express';
+
+const handler: BackgroundHandler = async (event: HandlerEvent, _context: HandlerContext) => {
+  const webhookData = JSON.parse(event.body || '');
+  const { orderId } = event.queryStringParameters || {};
+
+  const client = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
+
+  const { data: order } = await client.from('orders').select('*').eq('id', orderId).single();
+
+  const output = webhookData.output;
+  const res = await axios({ url: output, method: 'GET', responseType: 'arraybuffer' });
+
+  const printTemplate = await sharp(path.join(__dirname, '..', '..', 'greeting-card-7x5-template.png'))
+    .composite([
+      {
+        input: res.data,
+        top: 29,
+        left: 1528,
+      },
+      {
+        input: {
+          text: {
+            text: order.message,
+            font: 'Cutive-Regular',
+            fontfile: path.join(__dirname, '..', '..', 'Cutive-Regular.ttf'),
+            width: 1149,
+            dpi: 48,
+          },
+          top: 257,
+          left: 4761,
+        },
+      },
+    ])
+    .png()
+    .withMetadata({ density: 300 })
+    .toBuffer();
+
+  const storageKey = `print-assets/${orderId}.png`;
+
+  await client.storage.from('assets').upload(storageKey, printTemplate, {
+    contentType: 'image/png',
+  });
+
+  await client.from('orders').update({ print_asset_key: storageKey }).eq('id', orderId);
+
+  // eslint-disable-next-line no-console
+  console.log(`Stored print template for ${orderId}`);
+
+  const printAssetUrl = await client.storage.from('assets').createSignedUrl(storageKey, 60 * 60 * 24 * 7);
+
+  try {
+    // send off print job
+    const printRes = await axios.post(
+      `${process.env.PRODIGI_API_URL!}/v4.0/orders`,
+      {
+        callbackUrl: `${process.env.URL}/.netlify/functions/prodigi-update?orderId=${orderId}`,
+        merchantReference: orderId,
+        shippingMethod: SHIPPING_METHOD,
+        idempotencyKey: orderId,
+        recipient: {
+          name: order.name,
+          address: {
+            line1: order.line1,
+            line2: order.line2,
+            postalOrZipCode: order.postal_code,
+            townOrCity: order.city,
+            stateOrCounty: order.state,
+            countryCode: order.country,
+          },
+        },
+        items: [
+          {
+            sku: GREETING_CARD_SKU,
+            copies: 1,
+            sizing: 'fillPrintArea',
+            recipientCost: {
+              amount: '10.00',
+              currency: 'USD',
+            },
+            assets: [
+              {
+                printArea: 'default',
+                url: printAssetUrl.data?.signedUrl,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Api-Key': process.env.PRODIGI_API_KEY!,
+        },
+      }
+    );
+
+    if (printRes.status !== 200 || printRes.data.outcome.toLowerCase() !== 'created') {
+      // eslint-disable-next-line no-console
+      console.error(printRes.data);
+      throw new Error('could not create print job');
+    }
+
+    await client.from('orders').update({ prodigi_order_id: printRes.data.order.id }).eq('id', orderId);
+
+    // eslint-disable-next-line no-console
+    console.log(`Created print job for ${orderId}`);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    console.error(JSON.stringify(error.response?.data));
+    throw new Error('could not create print job');
+  }
+};
+
+export { handler };
